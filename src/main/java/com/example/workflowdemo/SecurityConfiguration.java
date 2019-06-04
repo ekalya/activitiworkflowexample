addee36ad0dcf3b2f1f55669e92d391684913f07
Elisha Kalya <ekalya@localhost.localdomain>
2019-06-04 08:14:50 -0600

    Initial Creation

---
+++ src/main/java/com/example/workflowdemo/SecurityConfiguration.java
@@ -0,0 +1,62 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.example.workflowdemo;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.authentication.dao.DaoAuthenticationProvider;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+
+@Configuration
+@EnableWebSecurity
+public class SecurityConfiguration
+        extends WebSecurityConfigurerAdapter {
+
+    @Autowired
+    UserDetailsServiceImpl userDetailsServiceImpl;
+
+    @Bean
+    public DaoAuthenticationProvider authProvider() {
+        DaoAuthenticationProvider authProvider = new DaoAuthenticationProvider();
+        authProvider.setUserDetailsService(userDetailsServiceImpl);
+        authProvider.setPasswordEncoder(passwordEncoder());
+        return authProvider;
+    }
+
+    private PasswordEncoder passwordEncoder() {
+        return new BCryptPasswordEncoder();
+    }
+
+    @Override
+    protected void configure(HttpSecurity http) throws Exception {
+//        http
+//                .csrf().disable()
+//                .authorizeRequests()
+//                .antMatchers("/").permitAll()
+//                .antMatchers("/h2_console/**").permitAll()
+//                .antMatchers("/workflowprocessess/**").permitAll()
+//                .anyRequest()
+//                .authenticated()
+//                .and()
+//                .httpBasic();
+
+        http.authorizeRequests()
+                .antMatchers("/").permitAll()
+                .antMatchers("/h2_console/**").permitAll()
+                .antMatchers("/api/**").permitAll()
+                .and()
+                .httpBasic();
+
+        http.csrf().disable();
+       // http.headers().frameOptions().disable();
+    }
+
+}
---
+++ src/main/java/com/example/workflowdemo/WorkflowDemoApplication.java
@@ -0,0 +1,53 @@
+package com.example.workflowdemo;
+
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.security.servlet.SecurityAutoConfiguration;
+import org.springframework.context.annotation.Bean;
+
+@SpringBootApplication(exclude = {SecurityAutoConfiguration.class})
+public class WorkflowDemoApplication {
+
+
+    public static void main(String[] args) {
+        SpringApplication.run(WorkflowDemoApplication.class, args);
+    }
+
+    @Bean
+    CommandLineRunner init() {
+
+        return new CommandLineRunner() {
+
+            public void run(String... strings) throws Exception {
+
+            }
+        };
+    }
+
+//    @Bean
+//    public CommandLineRunner run() throws Exception {
+//        return args -> {
+//            ProcessEngineConfiguration cfg = new StandaloneProcessEngineConfiguration()
+//                    .setJdbcUrl("jdbc:h2:mem:testdb;DB_CLOSE_DELAY=1000")
+//                    .setJdbcUsername("sa")
+//                    .setJdbcPassword("password")
+//                    .setJdbcDriver("org.h2.Driver")
+//                    .setDatabaseSchemaUpdate(ProcessEngineConfiguration.DB_SCHEMA_UPDATE_TRUE);
+//            ProcessEngine processEngine = cfg.buildProcessEngine();
+//            String pName = processEngine.getName();
+//            String ver = ProcessEngine.VERSION;
+//            System.out.println("ProcessEngine [" + pName + "] Version: [" + ver + "]");
+//
+//            RepositoryService repositoryService = processEngine.getRepositoryService();
+//            Deployment deployment = repositoryService.createDeployment()
+//                    .addClasspathResource("processes/onboarding.bpmn20.xml").deploy();
+//            ProcessDefinition processDefinition = repositoryService.createProcessDefinitionQuery()
+//                    .deploymentId(deployment.getId()).singleResult();
+//            System.out.println(
+//                    "Found process definition ["
+//                    + processDefinition.getName() + "] with id ["
+//                    + processDefinition.getId() + "]");
+//        };
+//    }
+}
